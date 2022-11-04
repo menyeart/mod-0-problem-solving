f68e490ef158986fc1f5b8d36807753b53e712bd
Matt Enyeart <menyeart1@gmail.com>
2022-11-04 11:38:55 -0600

    Add a solution to the challenge 1 exercise

---
+++ challenge1.rb
@@ -0,0 +1,16 @@
+# Challenge: Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+# Overall Goal: Create an array of strings, iterate over it, for each word, print out a version with all letters changed to lower case
+
+# Pseudocode
+#  - Create an array of strings with a mix of uppercase and lower case letters.
+#  - Use the each method to iterate through the array.
+#  - For each item use the downcase method to change all letters to lower case
+#  - Print each invidivdual item.
+
+array = ['aPPle','baNana', 'STRAWBERRY', 'orange', 'grapeFRUIT']
+
+array.each do |item|
+  puts item.downcase
+end
+
